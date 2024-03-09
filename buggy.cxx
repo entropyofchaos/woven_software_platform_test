4f3f317e0d3321e6b027eef2bfa6885aa7b79268
Brian Lichtman <entropyofchaos@gmail.com>
2024-03-10 00:12:40 +0900

    Updated project to now be thread safe and removed all pointer usage. Also cleaned up some random bugs

--- buggy.cxx
+++ buggy.cxx
@@ -1,27 +1,36 @@
+#include <algorithm>
+#include <chrono>
+#include <condition_variable>
+#include <cstdio>
+#include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
 
 struct Word
 {
-  char * data;
+  std::string data;
   int count;
 
-  Word ( char * data_ ) :
-    data( ::strdup(data_) )
+  Word (std::string data_ ) :
+    data(data_),
+    count(1)
   {}
   
   Word () :
-    data((char *)"")
+    data(std::string()), 
+    count(1)
   {}
 };
 
-static std::vector<Word*> s_wordsArray;
+static std::vector<Word> s_wordsArray;
 static Word s_word;
 static int s_totalFound;
 
+// Variables for thread syncronization safety
+static std::mutex s_mutex;
+static std::condition_variable s_condVar;
+static bool s_wordReady = false;
 
 // Worker thread: consume words passed from the main thread and insert them
 // in the 'word list' (s_wordsArray), while removing duplicates. Terminate when
@@ -33,29 +42,36 @@ static void workerThread ()
   
   while (!endEncountered)
   {
-    if (s_word.data[0]) // Do we have a new word?
-    {
-      Word * w = new Word(s_word); // Copy the word
-      
-      s_word.data[0] = 0; // Inform the producer that we consumed the word
-      
-      endEncountered = std::strcmp( s_word.data, "end" ) == 0;
-      
-      if (!endEncountered)
-      {
-        // Do not insert duplicate words
-        for ( auto p : s_wordsArray )
-        {
-          if (!std::strcmp( p->data, w->data ))
-          {
-            ++p->count;
-            found = true;
-            break;
-          }
-        }
+    // Protect the word to read
+    std::unique_lock<std::mutex> lock(s_mutex);
 
-        if (!found)
-          s_wordsArray.push_back( w );
+    // Do we have a new word?
+    s_condVar.wait(lock, [] { return s_wordReady; });
+
+    Word w(s_word); // Copy the word
+    s_wordReady = false; // Reset the flag to inform the producer that we consumed the word
+
+    s_condVar.notify_one(); // Wake up the producer
+    lock.unlock(); // Unlock the lock so the producer can start producing while we finish processing
+      
+    endEncountered = w.data.compare("end") == 0;
+      
+    if (!endEncountered)
+    {
+      // Do not insert duplicate words
+      for (auto &p : s_wordsArray)
+      {
+        if (!p.data.compare(w.data))
+        {
+          ++p.count;
+          found = true;
+          break;
+        }
+      }
+
+      if (!found)
+      {
+        s_wordsArray.push_back(w);
       }
     }
   }
@@ -69,23 +85,38 @@ static void readInputWords ()
 {
   bool endEncountered = false;
   
-  std::thread * worker = new std::thread( workerThread );
+  std::thread worker( workerThread );
 
-  char * linebuf = new char[32];
+  std::string linebuf;
   
   while (!endEncountered)
   {
-    if (!std::gets( linebuf )) // EOF?
-      return;
-    
-    endEncountered = std::strcmp( linebuf, "end" ) == 0;
+    if (!std::getline(std::cin, linebuf)) // EOF or error?
+    {
+        // If EOF or an error occurs, treat this the same as recieving end to
+        // handle this gracefully. However, leave EOF in the cin buffer so that
+        // the program ends.
+        linebuf = "end";
+    }
 
+    std::unique_lock<std::mutex> lock(s_mutex);
+    
     // Pass the word to the worker thread
-    std::strcpy( s_word.data, linebuf );
-    while (s_word.data[0]); // Wait for the worker thread to consume it
+    s_word.data = linebuf;
+    endEncountered = linebuf.compare("end") == 0;
+    
+    // Tell the worker thread there is a word and to wake up 
+    s_wordReady = true;
+    s_condVar.notify_one();
+
+    // Wait for the worker thread to consume the word
+    if (!endEncountered)
+    {
+        s_condVar.wait(lock, [] { return !s_wordReady; });
+    }
   }
 
-  worker->join(); // Wait for the worker to terminate
+  worker.join(); // Wait for the worker to terminate
 }
 
 // Repeatedly ask the user for a word and check whether it was present in the word list
@@ -94,23 +125,27 @@ static void readInputWords ()
 static void lookupWords ()
 {
   bool found;
-  char * linebuf = new char[32];
+  std::string linebuf;
     
   for(;;)
   {
+    found = false;
+
     std::printf( "\nEnter a word for lookup:" );
-    if (std::scanf( "%s", linebuf ) == EOF)
-      return;
+
+    if (!std::getline(std::cin, linebuf)) // EOF or error?
+    {
+      break;
+    }
 
     // Initialize the word to search for
-    Word * w = new Word();
-    std::strcpy( w->data, linebuf );
+    Word w(linebuf);
 
     // Search for the word
     unsigned i;
     for ( i = 0; i < s_wordsArray.size(); ++i )
     {
-      if (std::strcmp( s_wordsArray[i]->data, w->data ) == 0)
+      if (s_wordsArray[i].data.compare(w.data) == 0)
       {
         found = true;
         break;
@@ -120,14 +155,18 @@ static void lookupWords ()
     if (found)
     {
       std::printf( "SUCCESS: '%s' was present %d times in the initial word list\n",
-                   s_wordsArray[i]->data, s_wordsArray[i]->count );
+                   s_wordsArray[i].data.c_str(), s_wordsArray[i].count);
       ++s_totalFound;
     }
     else
-      std::printf( "'%s' was NOT found in the initial word list\n", w->data );
+      std::printf( "'%s' was NOT found in the initial word list\n", w.data.c_str());
   }
 }
 
+// Custom comparison function to put words in alphabetical order
+bool compareWords(const Word& a, const Word& b) {
+    return a.data < b.data;
+}
 int main ()
 {
   try
@@ -135,12 +174,12 @@ int main ()
     readInputWords();
     
     // Sort the words alphabetically
-    std::sort( s_wordsArray.begin(), s_wordsArray.end() );
+    std::sort(s_wordsArray.begin(), s_wordsArray.end(), compareWords);
 
     // Print the word list
     std::printf( "\n=== Word list:\n" );
-    for ( auto p : s_wordsArray )
-      std::printf( "%s %d\n", p->data, p->count );
+    for (auto p : s_wordsArray)
+      std::printf( "%s %d\n", p.data.c_str(), p.count);
 
     lookupWords();
 
